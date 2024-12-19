5ba974652922449d8b2f771d2ba0ac4f490b3faa
Evan McPherson <evanmcpherson98@yahoo.com>
2024-12-18 17:59:12 -0800

    show_network not appearing when project is run locally

--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -1,8 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+// dataset state variable to hold JSON data from WP
+const [dataset, setDataset] = useState([]);
+
+// URL for WP JSON REST endpoint
+
+const dataURL = "https://dev-headless-cms-powered-app.pantheonsite.io/wp-json/twentytwentyone-child/v1/movies";
+
+// useEffect() to get JSON data to populate dataset state variable
+useEffect(() => {
+  fetch(dataURL) //fetch() to load raw JSON string
+  .then(response => response.json()) //json () to convert raw string into json object
+  .then(data => setDataset(data)) // react state set function to populate data state var
+},[])
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +31,19 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList id = "movie-list">
+          <IonListHeader>Movies</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines = "inset" key ={index}>
+              <IonLabel>
+                <h3>{item.post_title}</h3>
+                <p>{item.movie_director}</p>
+                <p>{item.release_date}</p>
+                <img src = {item.guid}></img>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- photo-gallery/src/pages/Tab3.tsx
+++ photo-gallery/src/pages/Tab3.tsx
@@ -1,8 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+// dataset state variable to hold JSON data from WP
+const [dataset, setDataset] = useState([]);
+
+// URL for WP JSON REST endpoint
+
+const dataURL = "https://dev-headless-cms-powered-app.pantheonsite.io/wp-json/twentytwentyone-child/v1/shows";
+
+// useEffect() to get JSON data to populate dataset state variable
+useEffect(() => {
+  fetch(dataURL) //fetch() to load raw JSON string
+  .then(response => response.json()) //json () to convert raw string into json object
+  .then(data => setDataset(data)) // react state set function to populate data state var
+},[])
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,10 +31,23 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonList id = "show-list">
+          <IonListHeader>Shows</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines = "inset" key ={index}>
+              <IonLabel>
+                <h1>{item.post_title}</h1>
+                <h3>{item.show_creator}</h3>
+                <p>{item.release_date}</p>
+                <p>{item.show_network}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Tab3;
+
